2c72558a53e77a979bfb1df2a5df30c36d4f2749
Michael Ummels <michael@ummels.de>
2014-06-06 20:04:58 +0200

    Change style of code blocks
    Include highlight.js in theme

--- functions.php
+++ functions.php
@@ -1,15 +1,28 @@
 <?php
 
+function disable_lato() {
+    wp_dequeue_style('twentyfourteen-lato');
+}
+
 function load_fonts() {
     wp_register_style('merriweather', 'http://fonts.googleapis.com/css?family=Merriweather:400,700,400italic,700italic|Merriweather+Sans:400,700|Cousine');
     wp_enqueue_style('merriweather');
 }
 
-function disable_lato() {
-    wp_dequeue_style('twentyfourteen-lato');
+function load_highlight() {
+    wp_register_script('highlight',  get_stylesheet_directory_uri() . '/highlight.pack.js', false, '8.0', true);
+    wp_enqueue_script('highlight');
+    wp_register_style('highlight', get_stylesheet_directory_uri() . '/googlecode.css', false, '8.0', 'screen');
+    wp_enqueue_style('highlight');
 }
 
-add_action('wp_print_styles', 'load_fonts');
+function init_highlight() {
+    echo '<script>hljs.initHighlightingOnLoad();</script>' . "\n";
+}
+
 add_action('wp_enqueue_scripts', 'disable_lato', 11);
+add_action('wp_enqueue_scripts', 'load_fonts');
+add_action('wp_enqueue_scripts', 'load_highlight');
+add_action('wp_footer', 'init_highlight', 25);
 
 ?>
